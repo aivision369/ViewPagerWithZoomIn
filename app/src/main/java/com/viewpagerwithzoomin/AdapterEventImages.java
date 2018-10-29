15cfa37fbfdea6f6f493aef389def69376693b66
zennaxx.technology <zennaxx.tech@gmail.com>
2018-10-29 12:57:39 +0530

    add pageMargin and padding in the viewpager programmatically

--- app/src/main/java/com/viewpagerwithzoomin/AdapterEventImages.java
+++ app/src/main/java/com/viewpagerwithzoomin/AdapterEventImages.java
@@ -1,27 +1,25 @@
 package com.viewpagerwithzoomin;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-
-public class AdapterEventImages extends PagerAdapter
-{
-    public static final String TAG = "[AdapterEventImages]";
+public class AdapterEventImages extends PagerAdapter {
 
     private Context mContext;
-    Activity activity;
-    int [] imageList;
+    private int [] imageList;
+    private OnTouchListener onTouchListener;
+    private boolean isEnabledPagePadding;
 
-    public AdapterEventImages(Activity mContext, int [] imageList) {
+    protected AdapterEventImages(Context mContext, int[] imageList, OnTouchListener onTouchListener, boolean isEnabledPagePadding) {
         this.mContext = mContext;
-        activity = mContext;
         this.imageList = imageList;
+        this.onTouchListener = onTouchListener;
+        this.isEnabledPagePadding =isEnabledPagePadding;
     }
 
     @Override
@@ -31,17 +29,31 @@ public class AdapterEventImages extends PagerAdapter
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return view == ((RelativeLayout) object);
+        return view == object;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, final int position) {
         View itemView = LayoutInflater.from(mContext).inflate(R.layout.event_image_pager, container, false);
 
-        final ImageView image = (ImageView)itemView.findViewById(R.id.iv_event_pager);
+        final TouchImageView image = (TouchImageView)itemView.findViewById(R.id.iv_event_pager);
         image.setImageResource(imageList[position]);
-
         container.addView(itemView);
+        if (isEnabledPagePadding){
+            image.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (image.getCurrentZoom() > 1.0) {
+                        onTouchListener.onTouch(true);
+                    } else {
+                        onTouchListener.onTouch(false);
+                    }
+                    return false;
+                }
+
+            });
+        }
+
 
         return itemView;
     }
@@ -49,5 +61,9 @@ public class AdapterEventImages extends PagerAdapter
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((RelativeLayout) object);
+    }
+
+    interface  OnTouchListener{
+        void onTouch(boolean isZoom);
     }
 }
--- app/src/main/java/com/viewpagerwithzoomin/MainActivity.java
+++ app/src/main/java/com/viewpagerwithzoomin/MainActivity.java
@@ -1,23 +1,50 @@
 package com.viewpagerwithzoomin;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] imageList = {R.mipmap.bg1, R.mipmap.bg2, R.mipmap.bg3};
+    private ViewPager viewpagerImages;
+    AdapterEventImages adapterEventImages;
+    private boolean isEnabledPagePadding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExtendedViewPager viewpager_images = (ExtendedViewPager)findViewById(R.id.viewpager_event_images);
-        int []imageList = {R.mipmap.bg1, R.mipmap.bg2, R.mipmap.bg3};
+        viewpagerImages = (ViewPager) findViewById(R.id.viewpager_event_images);
+        isEnabledPagePadding = true;
+        setViewpagerSetting(!isEnabledPagePadding);
+        adapterEventImages = new AdapterEventImages(MainActivity.this, imageList, onTouchListener, isEnabledPagePadding);
+        viewpagerImages.setAdapter(adapterEventImages);
+        adapterEventImages.notifyDataSetChanged();
 
-        AdapterEventImages adapterEventImages = new AdapterEventImages(MainActivity.this, imageList);
-        if (adapterEventImages != null)
-        {
-            viewpager_images.setAdapter(adapterEventImages);
-            adapterEventImages.notifyDataSetChanged();
+    }
+
+    AdapterEventImages.OnTouchListener onTouchListener = new AdapterEventImages.OnTouchListener() {
+        @Override
+        public void onTouch(boolean isZoom) {
+            if (isZoom) {
+                setViewpagerSetting(true);
+            } else {
+                setViewpagerSetting(false);
+            }
+        }
+    };
+
+    private void setViewpagerSetting(boolean isZoomed) {
+        if (isZoomed) {
+            viewpagerImages.setPadding(0, 0, 0, 0);
+            viewpagerImages.setClipChildren(false);
+            viewpagerImages.setPageMargin(0);
+        } else {
+            viewpagerImages.setPadding(20, 0, 20, 0);
+            viewpagerImages.setClipChildren(false);
+            viewpagerImages.setPageMargin(30);
         }
     }
 }
